4748d713fb48d211fc105920ee8e37900e97088b
Xtarii <carlalvinelias@gmail.com>
2026-03-03 21:28:10 +0100

    print function
    - Not working. I haven't looked into how the video memory works yet. So it just "prints" to each position but the video memory is not a buffer in that way.
    
    So it is a fun function because it prints strings with the rainbow - Rainbow print

--- [KERNEL]/kernel.c
+++ [KERNEL]/kernel.c
@@ -1,4 +1,23 @@
+/**
+ * Prints a string to the video memory
+ */
+void print(const char*);
+
+
+
 void main() {
+    // char *video_memory = (char*)0xb8000;
+    // *video_memory = 'X';
+
+
+    print("Hello, world!");
+}
+
+
+
+void print(const char *str) {
     char *video_memory = (char*)0xb8000;
-    *video_memory = 'X';
+    for(int i = 0; str[i]; i++) {
+        video_memory[i] = str[i];
+    }
 }
