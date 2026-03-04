fd6f80557dfb91ec230a9faf41a449500114dfde
Xtarii <carlalvinelias@gmail.com>
2026-03-04 15:05:33 +0100

    test

--- [BOOT]/[boot].asm
+++ [BOOT]/[boot].asm
@@ -7,18 +7,13 @@ mov bp, 0x9000  ; Stack setup
 mov sp, bp
 
 
-mov bx, MESSAGE_RM
-call print
-call println
 
 call load_kernel
 call switch_to_protected
-jmp $   ; Should never be called
 
 
 
 %include "debug/print.asm"
-%include "debug/pprint.asm"
 %include "disk/disk.asm"
 %include "debug/hex.asm"
 %include "protected/gdp.asm"
@@ -31,10 +26,6 @@ jmp $   ; Should never be called
 ;
 [bits 16]
 load_kernel:
-    mov bx, MESSAGE_LOAD_KERNEL_RM
-    call print
-    call println
-
     mov bx, KERNEL_OFFSET
     mov dh, 2
     mov dl, [BOOT_DRIVE]
@@ -46,18 +37,11 @@ load_kernel:
 ;
 [bits 32]
 PM_ENTRYPOINT:
-    mov ebx, MESSAGE_PM
-    call pprint
     call KERNEL_OFFSET
-    jmp $   ; Loop when kernel returns controls
 
 
 
 BOOT_DRIVE db 0
-
-MESSAGE_RM db "Real mode (16 bit) loaded", 0
-MESSAGE_LOAD_KERNEL_RM db "Loading kernel into memory...", 0
-MESSAGE_PM db "Protected mode (32 bit) loaded", 0
 
 
 
