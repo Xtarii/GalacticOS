42470c6da80a1c55b79644a6a66d8dd1c9d0bbd6
Xtarii <carlalvinelias@gmail.com>
2026-03-09 16:57:02 +0100

    added int to ascii implementation
    - Added the one from the tutorial. I will change it in the future. I made one converter once it is not the funniest thing to do.

--- [KERNEL]/utilities/utils.c
+++ [KERNEL]/utilities/utils.c
@@ -8,4 +8,13 @@ void kmemcpy(void *src, void *dst, int bytes) {
 
 
 
-void itoa(int n, char *buffer) {}
+void itoa(int n, char *buffer) {
+    int i = 0, sign = 0;
+    if((sign = n) < 0) n = -n;
+    do {
+        buffer[i++] = n % 10 + '0';
+    }while((n /= 10) > 0);
+
+    if(sign < 10) buffer[i++] = '-';
+    buffer[i] = '\0';
+}
