e093c899736936f0d0af81d4b508ba6f36b5f695
Xtarii <carlalvinelias@gmail.com>
2026-03-09 17:38:54 +0100

    cleanup

--- [KERNEL]/kernel.c
+++ [KERNEL]/kernel.c
@@ -1,20 +1,10 @@
 #include "cpu/ISR.h"
 #include "drivers/screen.h"
-#include "utilities/utils.h"
 
 void main() {
     isr_init();
 
     clear();
-
-
-
-    char s[15];
-    itoa(15, s);
-    kprint(s);
-    kprint("\n\n");
-
-
 
     __asm__ __volatile__("int $2");
     __asm__ __volatile__("int $3");
