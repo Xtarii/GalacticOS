4a57e522ff11c570b2aa3ae4e49675cb65c6e0cd
Xtarii <carlalvinelias@gmail.com>
2026-03-10 16:45:31 +0100

    added a memory manager for the kernel for when I add paging

---
+++ [KERNEL]/memory/memory.c
@@ -0,0 +1,7 @@
+#include "memory.h"
+#include <stdint.h>
+
+/**
+ * Free memory address used for allocation memory
+ */
+extern uintptr_t free_memory_address;
---
+++ [KERNEL]/memory/memory.h
@@ -0,0 +1,6 @@
+#ifndef GALACTIC_MEMORY_H
+#define GALACTIC_MEMORY_H
+
+/* Memory allocation on kernel level here */
+
+#endif // GALACTIC_MEMORY_H
