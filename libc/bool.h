f866ab41ac25971a4b272ac3bffd561602cb2f9d
Xtarii <carlalvinelias@gmail.com>
2026-03-10 12:17:09 +0100

    added boolean

---
+++ libc/bool.h
@@ -0,0 +1,21 @@
+#ifndef BOOL_H
+#define BOOL_H
+
+/**
+ * Boolean type.
+ *
+ * Represents a 1 bit value that is either
+ * `1` or `0` - `true` or `false`
+ */
+typedef enum _bool {
+    /**
+     * A true value represented by `1`
+     */
+    true = 1,
+    /**
+     * A false value represented by `0`
+     */
+    false = 0
+} bool;
+
+#endif // BOOL_H
