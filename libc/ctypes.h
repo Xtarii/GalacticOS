5cbf7a1b1c15b8280a4879cccf0bf9a589d19e0f
Xtarii <carlalvinelias@gmail.com>
2026-03-10 11:50:56 +0100

    added CTypes

---
+++ libc/ctypes.h
@@ -0,0 +1,31 @@
+#ifndef CTYPES_H
+#define CTYPES_H
+
+/**
+ * Unsigned 32 bit integer
+ */
+typedef unsigned int uint32_t;
+/**
+ * Signed 32 bit integer
+ */
+typedef signed int int32_t;
+
+/**
+ * Unsigned 16 bit integer
+ */
+typedef unsigned short int uint16_t;
+/**
+ * Signed 16 bit integer
+ */
+typedef signed short int int16_t;
+
+/**
+ * Unsigned 8 bit integer
+ */
+typedef unsigned char uint8_t;
+/**
+ * Signed 8 bit integer
+ */
+typedef signed char int8_t;
+
+#endif // CTYPES_H
